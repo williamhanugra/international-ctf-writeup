df011c41ea9c9b346211734bfdf4895f3284ec09
Lu William Hanugra <hanugrawilliam@gmail.com>
2017-12-30 15:58:42 +0700

    megalal-solver-fix

--- 2017/Junior-34C3-CTF/Crypto/Megalal/megalal.py
+++ 2017/Junior-34C3-CTF/Crypto/Megalal/megalal.py
@@ -1,32 +1,72 @@
-from pwn import *
+#!/usr/bin/env python2
+
 import binascii
+import base64
+import random
 
-context.log_level='error'
+from secrets import g, p, x, h, flag
 
-chosen = int(binascii.hexlify("#overlord"), 16)
-role = binascii.unhexlify("{:02x}".format((chosen/2)))
+def xgcd(b, n):
+    x0, x1, y0, y1 = 1, 0, 0, 1
+    while n != 0:
+        q, b, n = b // n, n, b % n
+        x0, x1 = x1, x0 - q * x1
+        y0, y1 = y1, y0 - q * y1
+    return  b, x0, y0
 
-#STEP 1
-p = remote("35.197.255.108", 1337)
+def modinv(b, n):
+    g, x, _ = xgcd(b, n)
+    if g == 1:
+        return x % n
 
-p.recv()
-p.recv()
-p.sendline("2") #choose 2 for Registration
-p.recv()
-p.sendline("a") #send username
-p.recv()
-p.sendline(role) #send role
-acces_token = p.recv().split("\n")[1]
-c1,c2 = acces_token.split("_")
-new_acces_token = '{}_{:02x}'.format(c1, int(c2, 16) * 2)
+def enc(m):
+    M = int(binascii.hexlify(m), 16)
+    assert len(bin(M)) < len(bin(p)), 'm too long'
 
-#STEP 2
-p = remote("35.197.255.108", 1337)
+    y = random.SystemRandom().randint(0, p-1)
+    c1 = pow(g, y, p)
+    c2 = (M * pow(h, y, p) ) % p 
 
-print p.recv()
-print p.recv()
-p.sendline("1") #choose 1 for Login
-print p.recv()
-p.sendline(new_acces_token) #send new_acces_token
-print p.recvall()
+    return c1, c2
 
+def dec(c1, c2):
+    s = pow(c1, x, p)
+    M = (c2 * modinv(s, p)) % p
+    
+    return str(binascii.unhexlify('{:x}'.format(M)))
+
+def login():
+    c = raw_input('Please input your access token: ').split('_')
+    c1 = int(c[0], 16)
+    c2 = int(c[1], 16)
+    user, role = dec(c1, c2).split('#')
+    print('\nWelcome {}!'.format(user))
+    print('Your role is \'{}\'.\n'.format(role))
+    if role == 'overlord':
+        print('Here\'s your flag: {}'.format(flag))
+    print('That\'s all, nothing else happening here.')
+
+def register():
+    username = raw_input('Your username: ')
+    role = raw_input('Your role: ')
+
+    if role == 'overlord':
+        print('nope, you\'re not the overlord...')
+        return
+    c = enc('%s#%s' % (username, role))
+    token = '{:x}_{:x}'.format(c[0], c[1])
+
+    print('Here is your access token:\n{}'.format(token))
+
+if __name__ == '__main__':
+    print('What do you want to do?')
+    print('[1] Login')
+    print('[2] Register')
+    choice = raw_input('> ')
+    try:
+        if choice == '1':
+            login()
+        elif choice == '2':
+            register()
+    except:
+        print('something went wrong...')
---
+++ 2017/Junior-34C3-CTF/Crypto/Megalal/solver.py
@@ -0,0 +1,31 @@
+from pwn import *
+import binascii
+
+context.log_level='error'
+
+chosen = int(binascii.hexlify("#overlord"), 16)
+role = binascii.unhexlify("{:02x}".format((chosen/2)))
+
+#STEP 1
+p = remote("35.197.255.108", 1337)
+
+p.recv()
+p.recv()
+p.sendline("2") #choose 2 for Registration
+p.recv()
+p.sendline("a") #send username
+p.recv()
+p.sendline(role) #send role
+acces_token = p.recv().split("\n")[1]
+c1,c2 = acces_token.split("_")
+new_acces_token = '{}_{:02x}'.format(c1, int(c2, 16) * 2)
+
+#STEP 2
+p = remote("35.197.255.108", 1337)
+
+print p.recv()
+print p.recv()
+p.sendline("1") #choose 1 for Login
+print p.recv()
+p.sendline(new_acces_token) #send new_acces_token
+print p.recvall()
